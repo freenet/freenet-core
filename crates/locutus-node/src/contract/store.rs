59a59b4aa5e8e6d0447c009b11e9e44434575120
Ignacio Duart <iduartgomez@gmail.com>
2021-12-12 12:34:57 +0100

    Cache contracts in memory

--- crates/locutus-node/src/contract/store.rs
+++ crates/locutus-node/src/contract/store.rs
@@ -1,32 +1,47 @@
+use stretto::AsyncCache;
+
 use super::{Contract, ContractError, ContractKey};
 
 /// Handle contract blob storage on the file system.
-pub(crate) struct ContractStore {}
+pub(crate) struct ContractStore {
+    mem_cache: AsyncCache<ContractKey, Contract>,
+}
 
 impl ContractStore {
+    const MAX_MEM_CACHE: i64 = 10_000_000;
+    const MAX_NUM_CONTRACTS: usize = 10;
+
     pub fn new() -> Self {
-        Self {}
+        Self {
+            mem_cache: AsyncCache::new(10, Self::MAX_MEM_CACHE).expect("failed to build mem cache"),
+        }
     }
     /// Returns a copy of the contract bytes if available, none otherwise.
     pub async fn fetch_contract<CErr>(
         &self,
-        _key: &ContractKey,
+        key: &ContractKey,
     ) -> Result<Option<Contract>, ContractError<CErr>>
     where
         CErr: std::error::Error,
     {
-        todo!()
+        if let Some(contract) = self.mem_cache.get(key) {
+            Ok(Some(contract.as_ref().clone()))
+        } else {
+            todo!("fetch from disc")
+        }
     }
 
     /// Store a copy of the contract in the local store.
     pub async fn store_contract<CErr>(
         &mut self,
-        _contract: Contract,
+        contract: Contract,
     ) -> Result<(), ContractError<CErr>>
     where
         CErr: std::error::Error,
     {
-        // TODO
+        let key = contract.key();
+        let size = contract.data.len() as i64;
+        self.mem_cache.insert(key, contract, size).await;
         Ok(())
     }
 }
