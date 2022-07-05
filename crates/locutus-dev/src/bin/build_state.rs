b9dec7a3de80f4a7617b4a0f932ab0990c3eb9c8
Ignacio Duart <iduartgomez@gmail.com>
2022-07-06 00:18:47 +0200

    Bundle web state correctly

--- crates/locutus-dev/src/bin/build_state.rs
+++ crates/locutus-dev/src/bin/build_state.rs
@@ -1,6 +1,7 @@
-use std::{fs::File, io::Write, path::PathBuf};
 use byteorder::{BigEndian, WriteBytesExt};
 use clap::Parser;
+use std::{fs::File, io::Write, path::PathBuf};
+use tar::Header;
 
 use locutus_dev::{ContractType, StateConfig};
 
@@ -11,22 +12,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut complete_state = Vec::new();
 
     let source_path: PathBuf = cli.input_path;
-    let dest_file: PathBuf  = cli.output_file;
+    let dest_file: PathBuf = cli.output_file;
 
-    if let Some(ct) = cli.contract_type  {
-        match ct {
-            ContractType::Web => {
-                let _ = build_web_state(&mut complete_state, source_path, dest_file);
-            }
-            ContractType::Data => {
-                let _ = build_data_state(&mut complete_state, source_path, dest_file);
-            }
+    match cli.contract_type {
+        ContractType::Web => {
+            build_web_state(&mut complete_state, source_path, dest_file)?;
         }
-    };
+        ContractType::Data => {
+            build_data_state(&mut complete_state, source_path, dest_file)?;
+        }
+    }
     Ok(())
 }
 
-fn build_web_state(complete_state: &mut Vec<u8>, source_path: PathBuf, dest_file: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+fn build_web_state(
+    complete_state: &mut Vec<u8>,
+    source_path: PathBuf,
+    dest_file: PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Bundling web state from {source_path:?} into {dest_file:?}");
     append_metadata(complete_state)?;
     append_web_content(complete_state, source_path)?;
     let mut state = File::create(dest_file)?;
@@ -34,7 +38,12 @@ fn build_web_state(complete_state: &mut Vec<u8>, source_path: PathBuf, dest_file
     Ok(())
 }
 
-fn build_data_state(complete_state: &mut Vec<u8>, source_path: PathBuf, dest_file: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+fn build_data_state(
+    complete_state: &mut Vec<u8>,
+    source_path: PathBuf,
+    dest_file: PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Bundling data contract state from {source_path:?} into {dest_file:?}");
     append_metadata(complete_state)?;
     append_dynamic_state(complete_state, source_path)?;
     let mut state = File::create(dest_file)?;
@@ -48,26 +57,42 @@ fn append_metadata(state: &mut Vec<u8>) -> Result<(), Box<dyn std::error::Error>
     Ok(())
 }
 
-fn append_web_content(state: &mut Vec<u8>, source_path: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+fn append_web_content(
+    state: &mut Vec<u8>,
+    source_path: PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
     let web_tar = {
         let web_content = Vec::new();
-        let content_path = source_path;
         let mut tar = tar::Builder::new(web_content);
-        tar.append_path(content_path.join("index.html"))?;
-        tar.append_path(content_path.join("state.html"))?;
+        assert!(source_path.is_dir());
+        for path in &[
+            source_path.join("index.html"),
+            source_path.join("state.html"),
+        ] {
+            let data = File::open(path)?;
+            let mut header = Header::new_gnu();
+            header.set_metadata(&data.metadata()?);
+            tar.append(&header, data)?;
+        }
         tar.into_inner()?
     };
+    assert!(!web_tar.is_empty());
     let mut encoded = vec![];
     {
         let mut encoder = xz2::write::XzEncoder::new(&mut encoded, 6);
         encoder.write_all(&web_tar)?;
+        encoder.flush()?;
     }
+    assert!(!encoded.is_empty());
     state.write_u64::<BigEndian>(encoded.len() as u64)?;
     state.append(&mut encoded);
     Ok(())
 }
 
-fn append_dynamic_state(state: &mut Vec<u8>, source_path: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+fn append_dynamic_state(
+    state: &mut Vec<u8>,
+    source_path: PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
     let dynamic_state = source_path.join("initial_state.json");
     let json: serde_json::Value = serde_json::from_reader(File::open(dynamic_state)?)?;
     let mut bytes = serde_json::to_vec(&json)?;
