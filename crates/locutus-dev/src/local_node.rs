30d461873085bfee8750bb86ebb303fd80fa4ec5
Hector Santos <netsirius@gmail.com>
2022-07-16 00:26:46 +0200

    wip: fix data contract state treatment and add tests

--- crates/locutus-dev/src/local_node.rs
+++ crates/locutus-dev/src/local_node.rs
@@ -1,4 +1,5 @@
 use std::{collections::HashMap, sync::Arc};
+use log::log;
 
 use locutus_node::{
     either::Either, ClientError, ClientId, ClientRequest, HostResponse, HostResult, PeerKey,
@@ -160,6 +161,10 @@ impl LocalNode {
                 Ok(res)
             }
             ClientRequest::Update { key, delta } => {
+                println!("updated key: {:?}", key.encode());
+                for (key, params) in self.contract_params.iter() {
+                    log::info!("saved contract key {}", key.encode())
+                }
                 let parameters = {
                     match self
                         .contract_params
