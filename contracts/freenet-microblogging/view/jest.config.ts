011c86de8b0856a80350a0abb0463aa29bd82b22
Ignacio Duart <iduartgomez@gmail.com>
2022-08-02 09:49:49 +0200

    wip: use js stdlib in sample contract

--- contracts/freenet-microblogging/view/jest.config.ts
+++ contracts/freenet-microblogging/view/jest.config.ts
@@ -4,6 +4,6 @@ const config: Config.InitialOptions = {
 	transform: {
 		"^.+\\.tsx?$": "ts-jest",
 	},
-	roots: ["<rootDir>/tests", "<rootDir>/src"]
+	roots: ["<rootDir>/tests", "<rootDir>/web"]
 };
 export default config;
