eea6fda3caadacc26844f303b901588c2efb368c
Ian Clarke <ian.clarke@gmail.com>
2022-12-17 17:50:10 -0600

    Debugging issue sync
    try again
    
    try again
    
    try again
    
    try again
    
    try again
    
    try again
    
    try again
    
    try again
    
    try again
    
    try again
    
    try again
    
    try again
    
    try again

--- .github/scripts/pt_sync.py
+++ .github/scripts/pt_sync.py
@@ -15,23 +15,38 @@ headers = {
     "X-GitHub-Api-Version": "2022-11-28"
 }
 
-print("Get Github Issues")
-print(f"URL: {url}\nHeaders: {headers}\n")
-
 response = requests.get(url, headers=headers)
 
-print(f"Retrieved issues from Github API\n")
+# Report how many issues were found or the error if the request failed
+if response.status_code >= 200 and response.status_code <= 299:
+    print(f"Found {len(response.json()['items'])} issues with planned label on Github. Synchronizing to Pivotal Tracker")
+else:
+    print(f"Failed to search for issues: {response.content}")
+    exit(1)
+
+synchronized_count = 0
 
 # Iterate over the issues and synchronize them to Pivotal Tracker
 for issue in response.json()["items"]:
-    # Check if the issue has already been synced to Pivotal Tracker
-    if "external_id" in issue:
-        continue
+    # Check if there is already a comment containing a Pivotal Tracker story URL
+    comments_url = issue["comments_url"]
+    comments_response = requests.get(comments_url, headers=headers)
+    if comments_response.status_code >= 200 and comments_response.status_code <= 299:
+        continue_outer = False
+        for comment in comments_response.json():
+            if "Pivotal Tracker story" in comment["body"]:
+                continue_outer = True
+                break
+        if continue_outer:
+            continue
 
     title = issue["title"]
     body = issue["body"]
     url = issue["html_url"]
     issue_id = issue["id"]
+    issue_number = issue["number"]
+
+    print(f"Syncing issue {issue_number} to Pivotal Tracker: {title}")
 
     # Create a new story in the Pivotal Tracker project
     story_url = f"https://www.pivotaltracker.com/services/v5/projects/{PT_PROJECT_ID}/stories"
@@ -40,10 +55,38 @@ for issue in response.json()["items"]:
     }
     story_data = {
         "name": title,
-        "description": f"Original issue: {url}\n\n{body}"
+        "description": f"Original issue: {url}\n\nOriginal description (may be out of date):\n{body}"
     }
     story_response = requests.post(story_url, headers=story_headers, json=story_data)
+    # Verify status code is between 200 and 299 inclusive
+    if story_response.status_code < 200 or story_response.status_code > 299:
+        print(f"Failed to create story in Pivotal Tracker: {story_response}")
+        exit(1)
 
-    print(f"Created story in Pivotal Tracker\n")
-    print(f"Response: {story_response.content}\n")
+    story_json = story_response.json()
 
+    # Extract the Pivotal Tracker story ID and URL from the response
+    pt_story_id = story_json["id"]
+    pt_story_url = story_json["url"]
+
+    # Add a comment to the source Github issue with the Pivotal Tracker story URL
+    comment_url = f"https://api.github.com/repos/{GH_REPO}/issues/{issue_number}/comments"
+
+    print("Comment URL: " + comment_url + "\n")
+
+    comment_headers = {
+        "Authorization": f"Bearer {GH_ACCESS_TOKEN}",
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    comment_data = {
+        "body": f"Pivotal Tracker story: {pt_story_url}"
+        }
+    comment_response = requests.post(comment_url, headers=comment_headers, json=comment_data)
+    if comment_response.status_code != 201:
+        print(f"Failed to add comment to Github issue: {comment_response}")
+        exit(1)
+    
+    synchronized_count += 1
+
+print(f"Synchronized {synchronized_count} issues to Pivotal Tracker")
