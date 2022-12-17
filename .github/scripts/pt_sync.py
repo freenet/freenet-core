024902ba3a98dbaa7109b49dcfe27e721140c329
Ian Clarke <ian.clarke@gmail.com>
2022-12-17 17:50:10 -0600

    try again

--- .github/scripts/pt_sync.py
+++ .github/scripts/pt_sync.py
@@ -15,12 +15,14 @@ headers = {
     "X-GitHub-Api-Version": "2022-11-28"
 }
 
-print("Get Github Issues")
-print(f"URL: {url}\nHeaders: {headers}\n")
-
 response = requests.get(url, headers=headers)
 
-print(f"Retrieved issues from Github API\n")
+# Report how many issues were found or the error if the request failed
+if response.status_code == 200:
+    print(f"Found {len(response.json()['items'])} issues to sync")
+else:
+    print(f"Failed to search for issues: {response.content}", file=sys.stderr)
+    exit(1)
 
 # Iterate over the issues and synchronize them to Pivotal Tracker
 for issue in response.json()["items"]:
@@ -40,10 +42,33 @@ for issue in response.json()["items"]:
     }
     story_data = {
         "name": title,
-        "description": f"Original issue: {url}\n\n{body}"
+        "description": f"Original issue: {url}\n\nOriginal description (may be out of date):\n{body}"
     }
     story_response = requests.post(story_url, headers=story_headers, json=story_data)
+    if story_response.status_code != 201:
+        print(f"Failed to create story in Pivotal Tracker: {story_response.content}", file=sys.stderr)
+        continue
+
+    story_json = story_response.json()
 
     print(f"Created story in Pivotal Tracker\n")
     print(f"Response: {story_response.content}\n")
 
+    # Extract the Pivotal Tracker story ID and URL from the response
+    pt_story_id = story_json["id"]
+    pt_story_url = story_json["url"]
+
+    # Add a comment to the source Github issue with the Pivotal Tracker story URL
+    comment_url = f"https://api.github.com/repos/{GH_REPO}/issues/{issue_id}/comments"
+    comment_headers = {
+        "Authorization": f"Bearer {GH_ACCESS_TOKEN}",
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    comment_data = {
+        "body": f"Pivotal Tracker story: {pt_story_url}"
+        }
+    comment_response = requests.post(comment_url, headers=comment_headers, json=comment_data)
+    if comment_response.status_code != 201:
+        print(f"Failed to add comment to Github issue: {comment_response.content}", file=sys.stderr)
+        continue
