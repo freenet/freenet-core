411302c9257ae1bb9ae88f9cf2f56a5d41a81a62
Hector Santos <netsirius@gmail.com>
2023-03-22 09:08:44 +0100

    Update get_code_hash function

--- apps/freenet-email-app/web/src/bin/get_code_hash.rs
+++ apps/freenet-email-app/web/src/bin/get_code_hash.rs
@@ -1,30 +1,10 @@
-use std::env;
-use std::fs::File;
-use std::io::Read;
 use std::path::PathBuf;
-use locutus_stdlib::prelude::Parameters;
 
 const MANIFEST: &str = env!("CARGO_MANIFEST_DIR");
 
 fn main() {
     use locutus_dev::config::BuildToolCliConfig;
     use locutus_stdlib::prelude::ContractContainer;
-
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <key-id> <path-to-inbox-state>", args[0]);
-        std::process::exit(1);
-    }
-    println!("args: {:?}", args);
-    let key_id = &args[1];
-    let state_path = &args[2];
-
-    // Open the state file and read params content
-    let mut state_file = File::open(state_path).unwrap();
-    let mut buffer = Vec::new();
-    state_file.read_to_end(&mut buffer).unwrap();
-    let params = Parameters::from(buffer);
-
     let config = BuildToolCliConfig::default();
     let contract_path = PathBuf::from(MANIFEST).join("../contracts/inbox");
     eprintln!("inbox path: {contract_path:?}");
@@ -32,14 +12,14 @@ fn main() {
     locutus_dev::build::build_package(config, contract_path.as_path()).unwrap();
     let f = contract_path.join("build/locutus/freenet_email_inbox");
     eprintln!("wasm path: {f:?}");
-    let code_key = ContractContainer::try_from((f.as_path(), params))
+    let code_key = ContractContainer::try_from((f.as_path(), Vec::<u8>::new().into()))
         .unwrap()
         .unwrap_v1()
         .code()
         .hash_str();
     eprintln!("Inbox code hash: {code_key}");
     std::fs::write(
-        PathBuf::from(MANIFEST).join(format!("examples/inbox_code_hash_{}", key_id)),
+        PathBuf::from(MANIFEST).join("examples/inbox_code_hash"),
         code_key,
     )
         .unwrap();
