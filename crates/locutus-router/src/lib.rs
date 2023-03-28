146f737bff289b09fa122c6fadf545bc2d6e1357
Ian Clarke <ian.clarke@gmail.com>
2023-03-27 19:34:10 -0500

    another commit before rewrite

--- crates/locutus-router/src/lib.rs
+++ crates/locutus-router/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, time::Duration};
+use std::{collections::HashMap};
 
 use locutus_core::{libp2p::PeerId, Location};
 use pav_regression::{
@@ -27,16 +27,16 @@ impl PeerTimeEstimator {
             );
 
             all_points.push(point);
-            peer_points.entry(event.peer).or_insert_with(Vec::new).push(point);
+            peer_points.entry(event.peer).or_default().push(point);
         }
 
-        let global_regression = IsotonicRegression::new_ascending(all_points.as_slice());
+        let global_regression = IsotonicRegression::new_ascending(&all_points);
 
         let peer_regressions = peer_points
             .into_iter()
             .filter(|(_, points)| points.len() > MIN_PEER_POINTS_FOR_REGRESSION)
             .map(|(peer, points)| {
-                let regression = IsotonicRegression::new_ascending(points.as_slice());
+                let regression = IsotonicRegression::new_ascending(&points);
                 (peer, regression)
             })
             .collect();
@@ -55,21 +55,17 @@ impl PeerTimeEstimator {
 
         self.global_regression.add_points(&[point]);
 
-        if let Some(regression) = self.peer_regressions.get_mut(&event.peer) {
-            regression.add_points(&[point]);
-        } else {
-            let regression = IsotonicRegression::new_ascending(&[point]);
-            self.peer_regressions.insert(event.peer, regression);
-        }
+        self.peer_regressions
+            .entry(event.peer)
+            .or_insert_with(|| IsotonicRegression::new_ascending(&[point]))
+            .add_points(&[point]);
     }
 
     pub fn estimate_retrieval_time(&self, peer: PeerId, distance: f64) -> Option<f64> {
         if let Some(regression) = self.peer_regressions.get(&peer) {
-            let y = regression.interpolate(distance);
-            Some(y)
+            Some(regression.interpolate(distance))
         } else if self.global_regression.len() > MIN_PEER_POINTS_FOR_REGRESSION {
-            let y = self.global_regression.interpolate(distance);
-            Some(y)
+            Some(self.global_regression.interpolate(distance))
         } else {
             None
         }
@@ -88,21 +84,18 @@ pub struct RoutingEvent {
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     fn simulate_request(peer: PeerId, peer_location: Location, contract_location: Location) -> RoutingEvent {
-        let distance : f64 = peer_location.distance(&contract_location).into();
+        let distance: f64 = peer_location.distance(&contract_location).into();
 
         let result = distance.powf(0.5) + peer.to_bytes()[0] as f64;
-        let ev = RoutingEvent {
+        RoutingEvent {
             peer,
             peer_location,
             contract_location,
             result,
-        };
-        println!("Event: {ev:?}");
-        ev
+        }
     }
 
     #[test]
@@ -117,28 +110,29 @@ mod tests {
         }
 
         // Split the events into two sets
-        let (training_events, testing_events) = events.split_at(events.len()/2);
+        let (training_events, testing_events) = events.split_at(events.len() / 2);
 
         // Create a new estimator from the training set
         let estimator = PeerTimeEstimator::new(training_events.iter().cloned());
-
+        
         // Test the estimator on the testing set, recording the errors
         let mut errors = Vec::new();
         for event in testing_events {
-            let estimated_time = estimator.estimate_retrieval_time(event.peer, event.contract_location.distance(&event.peer_location).into());
+            let distance = event.contract_location.distance(&event.peer_location).into();
+            let estimated_time = estimator.estimate_retrieval_time(event.peer, distance);
             assert!(estimated_time.is_some());
             let estimated_time = estimated_time.unwrap();
             let actual_time = event.result;
             // print estimated and actual
-            println!("Estimated: {estimated_time:?}, Actual: {actual_time:?}");
+            println!("Estimated: {:?}, Actual: {:?}", estimated_time, actual_time);
             let error = (estimated_time - actual_time).abs();
+            errors.push(error);
         }
- 
-        println!("Error: {errors:?}"); 
+
+        println!("Errors: {:?}", errors);
 
         // Check that the errors are small
-        let average_error = errors.iter().sum::<i64>() as f64 / errors.len() as f64;
-        assert!(average_error < -100.0);
-
+        let average_error = errors.iter().sum::<f64>() / errors.len() as f64;
+        assert!(average_error < 0.01);
     }
 }
