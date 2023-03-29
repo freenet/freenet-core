96e3a4e59f9c701b5146ed0c44e5bba74b6d1e48
Ian Clarke <ian.clarke@gmail.com>
2023-03-28 21:43:33 -0500

    checkpoint

--- crates/locutus-router/src/lib.rs
+++ crates/locutus-router/src/lib.rs
@@ -1,3 +1,8 @@
 mod routing_outcome_estimator;
 
 use routing_outcome_estimator::RoutingOutcomeEstimator;
+
+struct PeerChooser {
+    time_est: RoutingOutcomeEstimator,
+    success_est: RoutingOutcomeEstimator,
+}
