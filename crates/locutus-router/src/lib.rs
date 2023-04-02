b91ce64765dc34a5954a05db1e8cd8e2753f18e2
Ian Clarke <ian.clarke@gmail.com>
2023-04-01 19:31:16 -0500

    wip

--- crates/locutus-router/src/lib.rs
+++ crates/locutus-router/src/lib.rs
@@ -1,8 +1,18 @@
 mod routing_outcome_estimator;
 
+use locutus_core::libp2p::PeerId;
 use routing_outcome_estimator::RoutingOutcomeEstimator;
 
 pub struct PeerChooser {
     time_est: RoutingOutcomeEstimator,
     success_est: RoutingOutcomeEstimator,
+}
+
+impl PeerChooser {
+
+}
+
+struct RoutingEvent {
+    peer_id : PeerId,
+    outcome : RoutingOutcome,
 }
