beddbfbbfb1e1eb5c396048cac780375fde7a64c
Ian Clarke <ian.clarke@gmail.com>
2023-04-18 11:58:30 -0500

    wip

--- crates/locutus-core/src/metering/mod.rs
+++ crates/locutus-core/src/metering/mod.rs
@@ -1,7 +1,15 @@
+use locutus_runtime::ContractInstanceId;
+
+use crate::ring::PeerKeyLocation;
+
 struct Meter {
 
 }
 
 enum AttributionSource {
+    /// The resources were consumed 
     Peer(PeerKeyLocation),
+
+    /// The resources were consumed by a contract we're relaying
+    Contract(ContractInstanceId),
 }
