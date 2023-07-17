69f9691d025df184b1bcea270b7fd95d51d41cac
Ian Clarke <ian.clarke@gmail.com>
2023-07-17 15:34:17 -0500

    wip, compiles

--- crates/locutus-stdlib/src/contract_interface/layer1.rs
+++ crates/locutus-stdlib/src/contract_interface/layer1.rs
@@ -1,4 +1,6 @@
-use super::*;
+use serde::{de::DeserializeOwned, Serialize};
+
+use crate::contract_interface as L0;
 
 pub trait ContractAdapter {
     // type Parameters: for<'x> From<&'x [u8]>;
@@ -13,32 +15,54 @@ pub trait ContractAdapter {
     fn validate_state(
         parameters: Self::Parameters,
         state: Self::State,
-        related: RelatedContracts<'static>,
-    ) -> Result<ValidateResult, ContractError>;
+        related: L0::RelatedContracts<'static>,
+    ) -> Result<L0::ValidateResult, L0::ContractError>;
 
     /// Verifies that the given `delta` is valid, given these `parameters`.
     fn validate_delta(
         parameters: Self::Parameters,
         delta: Self::StateDelta,
-    ) -> Result<bool, ContractError>;
+    ) -> Result<bool, L0::ContractError>;
 
     ///
     fn update_state(
         parameters: Self::Parameters,
         state: Self::State,
-        data: Vec<UpdateData<'static>>,
-    ) -> Result<L1UpdateModification<Self::State>, ContractError>;
+        data: Vec<L0::UpdateData<'static>>,
+    ) -> Result<L1UpdateModification<Self::State>, L0::ContractError>;
 
     fn summarize_state(
         parameters: Self::Parameters,
         state: Self::State,
-    ) -> Result<Self::StateSummary, ContractError>;
+    ) -> Result<Self::StateSummary, L0::ContractError>;
 
     fn get_state_delta(
         parameters: Self::Parameters,
         state: Self::State,
         summary: Self::StateSummary,
-    ) -> Result<Self::StateDelta, ContractError>;
+    ) -> Result<Self::StateDelta, L0::ContractError>;
+}
+pub enum UpdateData<'a, State, StateDelta>
+where
+    State: Serialize + DeserializeOwned,
+    StateDelta: Serialize + DeserializeOwned,
+{
+    State(State),
+    Delta(StateDelta),
+    StateAndDelta(State, StateDelta),
+    RelatedState {
+        related_to: L0::ContractInstanceId,
+        state: L0::State<'a>,
+    },
+    RelatedDelta {
+        related_to: L0::ContractInstanceId,
+        delta: L0::StateDelta<'a>,
+    },
+    RelatedStateAndDelta {
+        related_to: L0::ContractInstanceId,
+        state: L0::State<'a>,
+        delta: L0::StateDelta<'a>,
+    },
 }
 
 pub struct L1UpdateModification<State>
@@ -46,11 +70,11 @@ where
     State: Serialize + DeserializeOwned,
 {
     pub state: State,
-    pub related : Vec<RelatedContract>,
+    pub related: Vec<L0::RelatedContract>,
 }
 
 trait Encoder {
-    type Error: Into<ContractError>;
+    type Error: Into<L0::ContractError>;
     fn deserialize<T: DeserializeOwned>(bytes: &[u8]) -> Result<T, Self::Error>;
     fn serialize<T: Serialize>(value: &T) -> Result<Vec<u8>, Self::Error>;
 }
@@ -77,6 +101,15 @@ where
     }
 }
 
+impl From<Box<bincode::ErrorKind>> for L0::ContractError {
+    fn from(error: Box<bincode::ErrorKind>) -> Self {
+        // Convert the `Box<bincode::ErrorKind>` into a `ContractError` here.
+        // This will depend on the structure of `ContractError`.
+        // For instance, if ContractError has a variant that wraps around bincode error:
+        L0::ContractError::Deser(error.to_string())
+    }
+}
+
 /*
 This allows doing something like
 
@@ -93,15 +126,15 @@ impl ContractAdapter for MyContract {
 
 */
 
-impl<T> ContractInterface for T
+impl<T> L0::ContractInterface for T
 where
     T: Encoder + ContractAdapter,
 {
     fn validate_state(
-        parameters: Parameters<'static>,
-        state: State<'static>,
-        related: RelatedContracts<'static>,
-    ) -> Result<ValidateResult, ContractError> {
+        parameters: L0::Parameters<'static>,
+        state: L0::State<'static>,
+        related: L0::RelatedContracts<'static>,
+    ) -> Result<L0::ValidateResult, L0::ContractError> {
         let parameters: <Self as ContractAdapter>::Parameters =
             <Self as Encoder>::deserialize(parameters.as_ref()).map_err(Into::into)?;
         let state: <Self as ContractAdapter>::State =
@@ -112,9 +145,9 @@ where
     /// Verify that a delta is valid if possible, returns false if and only delta is
     /// definitely invalid, true otherwise.
     fn validate_delta(
-        parameters: Parameters<'static>,
-        delta: StateDelta<'static>,
-    ) -> Result<bool, ContractError> {
+        parameters: L0::Parameters<'static>,
+        delta: L0::StateDelta<'static>,
+    ) -> Result<bool, L0::ContractError> {
         let parameters: <Self as ContractAdapter>::Parameters =
             <Self as Encoder>::deserialize(parameters.as_ref()).map_err(Into::into)?;
         let delta: <Self as ContractAdapter>::StateDelta =
@@ -124,31 +157,27 @@ where
 
     /// Update the state to account for the new data
     fn update_state(
-        parameters: Parameters<'static>,
-        state: State<'static>,
-        data: Vec<UpdateData<'static>>,
-    ) -> Result<crate::contract_interface::UpdateModification<'static>, ContractError> {
+        parameters: L0::Parameters<'static>,
+        state: L0::State<'static>,
+        data: Vec<L0::UpdateData<'static>>,
+    ) -> Result<L0::UpdateModification<'static>, L0::ContractError> {
         let parameters: <Self as ContractAdapter>::Parameters =
             <Self as Encoder>::deserialize(parameters.as_ref()).map_err(Into::into)?;
         let state: <Self as ContractAdapter>::State =
             <Self as Encoder>::deserialize(state.as_ref()).map_err(Into::into)?;
-        let data: Vec<UpdateData<'static>> = data
+        let data: Vec<L0::UpdateData<'static>> = data
             .into_iter()
-            .map(|d| {
-                let d: UpdateData<'static> =
-                    <Self as Encoder>::deserialize(d.to_owned()).map_err(Into::into)?;
-                Ok(d)
-            })
-            .collect::<Result<Vec<UpdateData<'static>>, ContractError>>()?;
-        <Self as ContractAdapter>::update_state(parameters, state, data)
+            .map(|d| todo!())
+            .collect::<Result<Vec<L0::UpdateData<'static>>, L0::ContractError>>()?;
+        todo!()
     }
 
     /// Generate a concise summary of a state that can be used to create deltas
     /// relative to this state.
     fn summarize_state(
-        parameters: Parameters<'static>,
-        state: State<'static>,
-    ) -> Result<StateSummary<'static>, ContractError> {
+        parameters: L0::Parameters<'static>,
+        state: L0::State<'static>,
+    ) -> Result<L0::StateSummary<'static>, L0::ContractError> {
         let parameters: <Self as ContractAdapter>::Parameters =
             <Self as Encoder>::deserialize(parameters.as_ref()).map_err(Into::into)?;
         let state: <Self as ContractAdapter>::State =
@@ -160,10 +189,10 @@ where
     /// This along with [`Self::summarize_state`] allows flexible and efficient
     /// state synchronization between peers.
     fn get_state_delta(
-        parameters: Parameters<'static>,
-        state: State<'static>,
-        summary: StateSummary<'static>,
-    ) -> Result<StateDelta<'static>, ContractError> {
+        parameters: L0::Parameters<'static>,
+        state: L0::State<'static>,
+        summary: L0::StateSummary<'static>,
+    ) -> Result<L0::StateDelta<'static>, L0::ContractError> {
         todo!()
     }
 }
