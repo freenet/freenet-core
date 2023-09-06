aad4d1370ad902feb35ce2999a331d8ec842cdb4
Ignacio Duart <iduartgomez@gmail.com>
2023-09-06 13:12:52 +0200

    Include generated code by default

--- crates/locutus-stdlib/build.rs
+++ crates/locutus-stdlib/build.rs
@@ -1,11 +1,10 @@
 use std::process::Command;
 
 fn main() {
-    // TODO try to install flatc from https://github.com/google/flatbuffers and use it to compile schemas
     let status = Command::new("flatc")
         .arg("--rust")
         .arg("-o")
-        .arg("../../target/flatbuffers/")
+        .arg("src")
         .arg("../../schemas/flatbuffers/common.fbs")
         .arg("../../schemas/flatbuffers/client_request.fbs")
         .arg("../../schemas/flatbuffers/host_response.fbs")
@@ -13,4 +12,5 @@ fn main() {
     if let Err(err) = status {
         println!("failed compiling flatbuffers schema: {err}")
     }
+    // TODO try to install flatc from https://github.com/google/flatbuffers and use it to compile schemas
 }
