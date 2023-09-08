76394d06ec30fdfc761abef9de835cb35038a8c6
Hector Santos <netsirius@gmail.com>
2023-09-08 19:33:28 +0200

    Fix ts host response handler

--- apps/stdlib/jest.config.ts
+++ apps/stdlib/jest.config.ts
@@ -1,9 +1,13 @@
-import type { Config } from '@jest/types';
+import type {Config} from '@jest/types';
+
 const config: Config.InitialOptions = {
-	verbose: true,
-	transform: {
-		"^.+\\.tsx?$": "ts-jest",
-	},
-	roots: ["<rootDir>/tests", "<rootDir>/src"]
+    verbose: true,
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    roots: ["<rootDir>/tests", "<rootDir>/src"],
+    moduleNameMapper: {
+        "host-response": "<rootDir>/src/host-response/host-response.ts",
+    }
 };
 export default config;
