d5a413c9a32f9332d1758ec20acc70787326a53f
Hector Santos <netsirius@gmail.com>
2023-09-16 23:49:56 +0200

    Make the Freenet microblogging application work

--- apps/freenet-microblogging/contracts/posts/src/lib.rs
+++ apps/freenet-microblogging/contracts/posts/src/lib.rs
@@ -33,13 +33,13 @@ pub struct Post {
 }
 
 impl Post {
-    pub fn hash(&self) -> [u8; 64] {
+    pub fn hash(&self) -> [u8; 32] {
         let mut hasher = Blake3::new();
         hasher.update(self.author.as_bytes());
         hasher.update(self.title.as_bytes());
         hasher.update(self.content.as_bytes());
         let hash_val = hasher.finalize();
-        let mut key = [0; 64];
+        let mut key = [0; 32];
         key.copy_from_slice(&hash_val[..]);
         key
     }
@@ -67,7 +67,7 @@ impl<'a> From<&'a mut PostsFeed> for FeedSummary {
 
 #[serde_as]
 #[derive(Serialize, Deserialize, PartialEq, Eq, Hash, PartialOrd, Ord)]
-struct MessageSummary(#[serde_as(as = "[_; 64]")] [u8; 64]);
+struct MessageSummary(#[serde_as(as = "[_; 32]")] [u8; 32]);
 
 impl<'a> TryFrom<StateSummary<'a>> for MessageSummary {
     type Error = ContractError;
@@ -240,7 +240,7 @@ mod test {
 
     #[test]
     fn validate_state() -> Result<(), Box<dyn std::error::Error>> {
-        let json_bytes = r#"{
+        let state = r#"{
             "messages": [
                 {
                     "author": "IDG",
@@ -253,7 +253,6 @@ mod test {
         .as_bytes()
         .to_vec();
 
-        let state: Vec<u8> = get_test_state(json_bytes);
         let valid = PostsFeed::validate_state(
             [].as_ref().into(),
             State::from(state),
@@ -283,28 +282,27 @@ mod test {
 
     #[test]
     fn update_state() -> Result<(), Box<dyn std::error::Error>> {
-        let state_bytes = r#"{"messages":[{"author":"IDG","content":"...",
+        let state = r#"{"messages":[{"author":"IDG","content":"...",
         "date":"2022-05-10T00:00:00Z","title":"Lore ipsum"}]}"#
             .as_bytes()
             .to_vec();
-        let state: Vec<u8> = get_test_state(state_bytes);
 
         let _delta =
             r#"[{"author":"IDG","content":"...","date":"2022-06-15T00:00:00Z","title":"New msg"}]"#;
 
         let delta = StateDelta::from(vec![
-            123u8, 10, 9, 9, 9, 34, 97, 117, 116, 104, 111, 114, 34, 58, 32, 34, 73, 68, 71, 34,
-            44, 10, 9, 9, 9, 34, 100, 97, 116, 101, 34, 58, 32, 34, 50, 48, 50, 50, 45, 48, 53, 45,
-            49, 48, 84, 48, 48, 58, 48, 48, 58, 48, 48, 90, 34, 44, 10, 9, 9, 9, 34, 116, 105, 116,
-            108, 101, 34, 58, 32, 34, 76, 111, 114, 101, 32, 105, 112, 115, 117, 109, 34, 44, 10,
-            9, 9, 9, 34, 99, 111, 110, 116, 101, 110, 116, 34, 58, 32, 34, 76, 111, 114, 101, 109,
-            32, 105, 112, 115, 117, 109, 32, 100, 111, 108, 111, 114, 32, 115, 105, 116, 32, 97,
-            109, 101, 116, 44, 32, 99, 111, 110, 115, 101, 99, 116, 101, 116, 117, 114, 32, 97,
-            100, 105, 112, 105, 115, 99, 105, 110, 103, 32, 101, 108, 105, 116, 44, 32, 115, 101,
-            100, 32, 100, 111, 32, 101, 105, 117, 115, 109, 111, 100, 32, 116, 101, 109, 112, 111,
-            114, 32, 105, 110, 99, 105, 100, 105, 100, 117, 110, 116, 32, 117, 116, 32, 108, 97,
-            98, 111, 114, 101, 32, 101, 116, 32, 100, 111, 108, 111, 114, 101, 32, 109, 97, 103,
-            110, 97, 32, 97, 108, 105, 113, 117, 97, 46, 34, 10, 9, 9, 125,
+            91, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 123, 10,
+            32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+            34, 97, 117, 116, 104, 111, 114, 34, 58, 34, 72, 83, 34, 44, 10, 32, 32, 32, 32, 32,
+            32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 34, 100, 97, 116,
+            101, 34, 58, 34, 50, 48, 50, 50, 45, 48, 54, 45, 49, 53, 84, 48, 48, 58, 48, 48, 58,
+            48, 48, 90, 34, 44, 10, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+            32, 32, 32, 32, 32, 32, 34, 116, 105, 116, 108, 101, 34, 58, 34, 78, 101, 119, 32, 109,
+            115, 103, 34, 44, 10, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+            32, 32, 32, 32, 32, 32, 34, 99, 111, 110, 116, 101, 110, 116, 34, 58, 34, 76, 111, 114,
+            101, 32, 105, 112, 115, 117, 109, 32, 46, 46, 46, 34, 10, 32, 32, 32, 32, 32, 32, 32,
+            32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 125, 10, 32, 32, 32, 32, 32, 32, 32, 32,
+            32, 32, 93,
         ]);
 
         let new_state = PostsFeed::update_state(
@@ -320,19 +318,17 @@ mod test {
                 "messages": [
                     {
                         "author": "IDG",
-                        "date": "2022-05-10T00:00:00Z",
                         "mod_msg": false,
-                        "signature": Value::Null,
+                        "signature": null,
                         "title": "Lore ipsum",
                         "content": "..."
                     },
                     {
-                        "author": "IDG",
-                        "date": "2022-06-15T00:00:00Z",
+                        "author": "HS",
                         "mod_msg": false,
-                        "signature": Value::Null,
+                        "signature": null,
                         "title": "New msg",
-                        "content": "..."
+                        "content": "Lore ipsum ..."
                     }
                 ]
             })
@@ -342,7 +338,7 @@ mod test {
 
     #[test]
     fn summarize_state() -> Result<(), Box<dyn std::error::Error>> {
-        let state_bytes = r#"{
+        let state = r#"{
             "messages": [
                 {
                     "author": "IDG",
@@ -355,23 +351,15 @@ mod test {
         .as_bytes()
         .to_vec();
 
-        let state: Vec<u8> = get_test_state(state_bytes);
-        let summary = PostsFeed::summarize_state([].as_ref().into(), State::from(state))?;
-        assert_eq!(
-            serde_json::from_slice::<serde_json::Value>(summary.as_ref()).unwrap(),
-            serde_json::json!([{
-                "author": "IDG",
-                "date": "2022-05-10T00:00:00Z",
-                "title": "Lore ipsum",
-                "content": "..."
-            }])
-        );
+        let posts_feed = PostsFeed::summarize_state([].as_ref().into(), State::from(state))?;
+        let feed_summary = serde_json::from_slice::<FeedSummary>(posts_feed.as_ref()).unwrap();
+        assert_eq!(feed_summary.summaries.len(), 1);
         Ok(())
     }
 
     #[test]
     fn get_state_delta() -> Result<(), Box<dyn std::error::Error>> {
-        let state_bytes = r#"{
+        let state = r#"{
             "messages": [
                 {
                     "author": "IDG",
@@ -380,7 +368,7 @@ mod test {
                     "content": "..."
                 },
                 {
-                    "author": "IDG",
+                    "author": "HS",
                     "date": "2022-04-10T00:00:00Z",
                     "title": "Lore ipsum",
                     "content": "..."
@@ -389,13 +377,22 @@ mod test {
         }"#
         .as_bytes()
         .to_vec();
-        let state: Vec<u8> = get_test_state(state_bytes);
-        let summary = serde_json::json!([{
-                "author": "IDG",
-                "date": "2022-04-10T00:00:00Z",
-                "title": "Lore ipsum",
-                "content": "..."
-        }]);
+
+        let summary_state = r#"{
+            "messages": [
+                {
+                    "author": "IDG",
+                    "date": "2022-05-11T00:00:00Z",
+                    "title": "Lore ipsum",
+                    "content": "..."
+                }
+            ]
+        }"#
+        .as_bytes()
+        .to_vec();
+
+        let mut summary_posts = PostsFeed::try_from(State::from(summary_state))?;
+        let summary = FeedSummary::from(&mut summary_posts);
 
         let delta = PostsFeed::get_state_delta(
             [].as_ref().into(),
@@ -405,10 +402,11 @@ mod test {
         assert_eq!(
             serde_json::from_slice::<serde_json::Value>(delta.as_ref()).unwrap(),
             serde_json::json!([{
-                "author": "IDG",
-                "date": "2022-05-11T00:00:00Z",
+                "author": "HS",
+                "mod_msg": false,
+                "signature": null,
                 "title": "Lore ipsum",
-                "content": "..."
+                "content": "...",
             }])
         );
         Ok(())
