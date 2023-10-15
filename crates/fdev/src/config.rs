f91c6f6eecc794dfe01df7c57768f37702b80104
KristijanZic <kristijan.zic@gmail.com>
2023-10-16 00:17:04 +0200

    Rename fdev clap name from 'Locutus' to 'Freenet'

--- crates/fdev/src/config.rs
+++ crates/fdev/src/config.rs
@@ -6,7 +6,7 @@ use freenet::dev_tool::OperationMode;
 use semver::Version;
 
 #[derive(clap::Parser, Clone)]
-#[clap(name = "Locutus Development Tool")]
+#[clap(name = "Freenet Development Tool")]
 #[clap(author = "The Freenet Project Inc.")]
 #[clap(version = "0.0.3")]
 pub struct Config {
