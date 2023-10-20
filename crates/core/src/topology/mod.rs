3f7f9fbd0eada22a9b48ddc57ceade116e0d1f28
Ian Clarke <ian.clarke@gmail.com>
2023-10-20 12:57:59 -0500

    wip

--- crates/core/src/topology/mod.rs
+++ crates/core/src/topology/mod.rs
@@ -10,11 +10,10 @@ use crate::ring::*;
 
 use self::small_world_rand::random_link_distance;
 
-const DEFAULT_MIN_DISTANCE: f64 = 0.01;
+const DEFAULT_MIN_DISTANCE: f64 = 1.0 / 1_000.0;
 
 pub(crate) enum TopologyStrategy {
     Random,
-    SmallWorld,
     LoadBalancing,
 }
 
@@ -32,9 +31,6 @@ impl TopologyStrategy {
     ) -> JoinTargetInfo {
         match self {
             TopologyStrategy::Random => random_strategy(my_location, peer_statistics),
-            TopologyStrategy::SmallWorld => {
-                small_world_metric_strategy(my_location, peer_statistics)
-            }
             TopologyStrategy::LoadBalancing => {
                 load_balancing_strategy(my_location, peer_statistics)
             }
