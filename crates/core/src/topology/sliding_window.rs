65c337f1fb21d3447fbd5f7ffc6febd856b10269
Ian Clarke <ian.clarke@gmail.com>
2023-10-30 21:58:02 -0500

    more improvements, tests pass

--- crates/core/src/topology/sliding_window.rs
+++ crates/core/src/topology/sliding_window.rs
@@ -1,11 +1,13 @@
 use std::collections::{BTreeMap, LinkedList};
-
+use thiserror::Error;
 use crate::ring::{Location, Distance};
 
+/// Sliding window data structure for calculating location density in a ring
 struct SlidingWindow {
     ordered_map: BTreeMap<Location, usize>,
     list: LinkedList<Location>,
     window_size: usize,
+    samples: usize,
 }
 
 impl SlidingWindow {
@@ -14,10 +16,13 @@ impl SlidingWindow {
             ordered_map: BTreeMap::new(),
             list: LinkedList::new(),
             window_size,
+            samples: 0,
         }
     }
 
     pub fn sample(&mut self, value: Location) {
+        self.samples += 1;
+
         self.list.push_back(value);
         *self.ordered_map.entry(value).or_insert(0) += 1;
 
@@ -33,11 +38,19 @@ impl SlidingWindow {
         }
     }
 
-    pub fn density(&self, position: Location, window_radius: usize) -> Option<Distance> {
+    pub fn density(&self, position: Location, window_radius: usize) -> std::result::Result<Distance, DensityError> {
+        if window_radius > self.samples / 2 || window_radius > self.window_size / 2 {
+            return Err(DensityError::WindowTooBig {
+                samples: self.samples,
+                window_size: self.window_size,
+            });
+        }
+
         // Create ranges for lower and upper bounds
         let lower_range = (std::ops::Bound::Unbounded::<Location>, std::ops::Bound::Included(position));
         let upper_range = (std::ops::Bound::Excluded(position), std::ops::Bound::Unbounded::<Location>);
 
+        // Iterate down from position accumulating the count to obtain the lower bound after window_radius samples
         let lower_iter = self.ordered_map.range(lower_range).rev();
         // Lower iter size is the total of the values in lower_iter
         let lower_iter_sz = lower_iter.map(|(_, v)| v).sum::<usize>();
@@ -45,9 +58,6 @@ impl SlidingWindow {
         let lower_iter = self.ordered_map.range(lower_range).rev();
 
         let lower_bound : Option<Location> = if lower_iter_sz >= window_radius {
-            // Fold over lower_iter through window_radius sample Locations to obtain the lower bound sample Location,
-            // bearing in mind that the map entry values are the number of times that the sample Location has been sampled
-    
             let mut count = 0;
             let mut lower_bound = None;
             for (sample_location, sample_count) in lower_iter {
@@ -59,7 +69,8 @@ impl SlidingWindow {
             }
             lower_bound
         } else {
-            // Iterate down from the top of the map accumulating the count to obtain the lower bound after window_radius-lower_iter_sz samples
+            // Wrap around to the top of the map and iterate down from the top of the map accumulating the 
+            // count to obtain the lower bound after window_radius-lower_iter_sz samples
             let mut count = lower_iter_sz;
             let mut lower_bound = None;
             for (sample_location, sample_count) in self.ordered_map.iter().rev() {
@@ -72,15 +83,12 @@ impl SlidingWindow {
             lower_bound
         };
 
-        // Now determine the upper_bound in the same way but moving forward rather than reverse
+        // Iterate up from position accumulating the count to obtain the upper bound after window_radius samples
         let upper_iter = self.ordered_map.range(upper_range);
         let upper_iter_sz = upper_iter.map(|(_, v)| v).sum::<usize>();
         let upper_iter = self.ordered_map.range(upper_range);
 
         let upper_bound : Option<Location> = if upper_iter_sz >= window_radius {
-            // Fold over upper_iter through window_radius sample Locations to obtain the upper bound sample Location,
-            // bearing in mind that the map entry values are the number of times that the sample Location has been sampled
-    
             let mut count = 0;
             let mut upper_bound = None;
             for (sample_location, sample_count) in upper_iter {
@@ -92,7 +100,8 @@ impl SlidingWindow {
             }
             upper_bound
         } else {
-            // Iterate down from the top of the map accumulating the count to obtain the upper bound after window_radius-upper_iter_sz samples
+            // Wrap around to the bottom of the map and iterate up from the bottom of the map accumulating the
+            // count to obtain the upper bound after window_radius-upper_iter_sz samples
             let mut count = upper_iter_sz;
             let mut upper_bound = None;
             for (sample_location, sample_count) in self.ordered_map.iter() {
@@ -108,12 +117,25 @@ impl SlidingWindow {
         // Now we have the lower and upper bounds, we can calculate the density which is
         // the distance between them
         return match (lower_bound, upper_bound) {
-            (Some(lower), Some(upper)) => Some(lower.distance(upper)),
-            _ => None,
+            (Some(lower), Some(upper)) => Ok(lower.distance(upper)),
+            _ => Err(DensityError::CantFindBounds),
         }
 
     }    
     
+}
+
+// Define the custom error type using thiserror
+#[derive(Error, Debug)]
+pub enum DensityError {
+    #[error("Not enough samples to determine lower and upper bounds")]
+    CantFindBounds,
+
+    #[error("Window radius too big. Window radius should be <= 50% of the number of samples ({samples}) and window size ({window_size}).")]
+    WindowTooBig {
+        samples: usize,
+        window_size: usize,
+    },
 }
 
 // Unit tests
@@ -145,7 +167,7 @@ mod tests {
         assert_eq!(sw.ordered_map.contains_key(&Location::new(0.4)), false);
 
         // Verify density
-        assert_eq!(sw.density(Location::new(0.65), 1), Some(Location::new(0.6).distance(Location::new(0.7))));
+        assert_eq!(sw.density(Location::new(0.65), 1).unwrap(), Location::new(0.6).distance(Location::new(0.7)));
     }
 
     #[test]
@@ -165,7 +187,7 @@ mod tests {
         sw.sample(Location::new(0.8));
         sw.sample(Location::new(0.9)); // <-- bottom of range
 
-        assert_eq!(sw.density(Location::new(0.55), 2), Some(Location::new(0.9).distance(Location::new(0.7))));
+        assert_eq!(sw.density(Location::new(0.55), 2).unwrap(), Location::new(0.9).distance(Location::new(0.7)));
     }
 
     #[test]
@@ -189,6 +211,6 @@ mod tests {
         assert_eq!(sw.ordered_map.contains_key(&Location::new(0.4)), false);
 
         // Verify density
-        assert_eq!(sw.density(Location::new(0.65), 2), Some(Location::new(0.6).distance(Location::new(0.8))));
+        assert_eq!(sw.density(Location::new(0.65), 2).unwrap(), Location::new(0.6).distance(Location::new(0.8)));
     }
 }
