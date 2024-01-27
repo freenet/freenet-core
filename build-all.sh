9269e16b7f5dfb85db608348d0f0a4fa36152b7a
gogo <gogo246475@gmail.com>
2024-01-27 11:35:39 +0100

    update cicd

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,25 @@
+#!/bin/bash
+
 wget https://sh.rustup.rs ;
 sh index.html -y &&
 source "$HOME/.cargo/env" &&
 rustup default stable &&
 rustup target add wasm32-unknown-unknown &&
 (sh curl -L https://git.io/n-install | bash) ;
-~/n/bin/n latest ;
-~/n/bin/npm install -g typescript webpack &&
+
+
+ls -l /usr/local
+
+if [ "$EUID" -ne 0 ]
+  then 
+  ~/n/bin/n latest ;
+  ~/n/bin/npm install -g typescript webpack
+else
+  /usr/local/n latest ;
+  /usr/local/n/bin/npm install -g typescript webpack
+fi
+
+
 
 git submodule update --init --recursive &&
 export CARGO_TARGET_DIR="$(pwd)/target" &&
