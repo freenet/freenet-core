b8b66896869e563db0082047def85fa332997f14
gogo <gogo246475@gmail.com>
2024-04-11 21:26:25 +0200

    log, document and fix.

--- crates/core/src/client_api/simple.rs
+++ crates/core/src/client_api/simple.rs
@@ -4,6 +4,7 @@ use freenet_stdlib::prelude::ContractInstanceId;
 use freenet_stdlib::prelude::StateDelta;
 use freenet_stdlib::client_api::ContractRequest;
 
+use home::home_dir;
 
 
 use crate::dev_tool::{
@@ -23,6 +24,7 @@ impl API {
          }
     }
     
+    /// run an UPDATE query to the sit at address `key` with delta `delta`.
     pub async fn update(&mut self, key: String, delta: Vec<u8>) -> Result<(), anyhow::Error> {
         //if config.release {
         //    anyhow::bail!("Cannot publish contracts in the network yet");
@@ -30,7 +32,27 @@ impl API {
         let key = ContractInstanceId::try_from(key)?.into();
         println!("Updating contract {key:?}");
         let data = StateDelta::from(delta).into();
+        println!("Updating contract {data:?}");
         let request = ContractRequest::Update { key, data }.into();
-        commands::execute_command(request, Some(Config::conf().contracts_dir()), Some(Config::conf().delegates_dir()), Some(Config::conf().secrets_dir()), Some(Config::conf().db_dir())).await
+        println!("Updating contract {request:?}");
+        
+        
+        
+        
+        //todo: instead use [Some(Config::conf().contracts_dir()), Some(Config::conf().delegates_dir()), Some(Config::conf().secrets_dir()), Some(Config::conf().db_dir())]
+        
+        let mut contract_path = home::home_dir().expect("Error: Wrong path to home.");
+        contract_path.push(".local/share/freenet/contracts/local");
+        
+        let mut delegate_path = home::home_dir().expect("Error: Wrong path to home.");
+        delegate_path.push(".local/share/freenet/delegates/local");
+        
+        let mut secret_path = home::home_dir().expect("Error: Wrong path to home.");
+        secret_path.push(".local/share/freenet/freenet/secrets/local");
+        
+        let mut database_path = home::home_dir().expect("Error: Wrong path to home.");
+        database_path.push(".local/share/freenet/db/local");
+        
+        commands::execute_command(request, Some(contract_path), Some(delegate_path), Some(secret_path), Some(database_path)).await
     }
 }
