32ad24e2043918b147f8d39a1b30c39a7ab8ed47
Al Liu <scygliu1@gmail.com>
2024-04-24 21:52:10 +0800

    Basic implementation

--- crates/core/src/bin/freenet.rs
+++ crates/core/src/bin/freenet.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use freenet::local_node::{Executor, OperationMode, PeerCliConfig};
+use freenet::{
+    local_node::{Executor, OperationMode, PeerCliConfig},
+    server::{local_node::run_local_node, network_node::run_network_node},
+};
 use std::net::SocketAddr;
 
 type DynError = Box<dyn std::error::Error + Send + Sync + 'static>;
@@ -7,7 +10,7 @@ type DynError = Box<dyn std::error::Error + Send + Sync + 'static>;
 async fn run(config: PeerCliConfig) -> Result<(), DynError> {
     match config.mode {
         OperationMode::Local => run_local(config).await,
-        OperationMode::Network => Err("network mode not yet enabled".into()),
+        OperationMode::Network => run_network(config).await,
     }
 }
 
@@ -17,7 +20,16 @@ async fn run_local(config: PeerCliConfig) -> Result<(), DynError> {
     freenet::config::Config::set_op_mode(OperationMode::Local);
     let executor = Executor::from_config(config, None).await?;
     let socket: SocketAddr = (ip, port).into();
-    freenet::server::local_node::run_local_node(executor, socket).await
+    run_local_node(executor, socket).await
+}
+
+async fn run_network(config: PeerCliConfig) -> Result<(), DynError> {
+    let port = config.port;
+    let ip = config.address;
+    freenet::config::Config::set_op_mode(OperationMode::Network);
+
+    // TODO: Get the peer id from somewhere
+    run_network_node(config, (ip, port).into(), panic!("")).await
 }
 
 fn main() -> Result<(), DynError> {
