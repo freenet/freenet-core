de5baea86aeae8b8c615ca9a48fabecf9e91756b
Ignacio Duart <iduartgomez@gmail.com>
2024-05-23 00:05:12 +0200

    More test cases

--- crates/core/src/transport/peer_connection.rs
+++ crates/core/src/transport/peer_connection.rs
@@ -129,14 +129,15 @@ impl PeerConnection {
         // listen for incoming messages or receipts or wait until is time to do anything else again
         let mut resend_check = Some(tokio::time::sleep(tokio::time::Duration::from_secs(1)));
 
-        #[cfg(debug_assertions)]
-        const KEEP_ALIVE_INTERVAL: Duration = Duration::from_secs(2);
-        #[cfg(not(debug_assertions))]
+        // #[cfg(debug_assertions)]
+        // const KEEP_ALIVE_INTERVAL: Duration = Duration::from_secs(2);
+        // #[cfg(not(debug_assertions))]
         const KEEP_ALIVE_INTERVAL: Duration = Duration::from_secs(20);
-        #[cfg(debug_assertions)]
-        const KILL_CONNECTION_AFTER: Duration = Duration::from_secs(6);
-        #[cfg(not(debug_assertions))]
+        // #[cfg(debug_assertions)]
+        // const KILL_CONNECTION_AFTER: Duration = Duration::from_secs(6);
+        // #[cfg(not(debug_assertions))]
         const KILL_CONNECTION_AFTER: Duration = Duration::from_secs(60);
+
         let mut keep_alive = tokio::time::interval(KEEP_ALIVE_INTERVAL);
         keep_alive.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Skip);
         keep_alive.tick().await;
