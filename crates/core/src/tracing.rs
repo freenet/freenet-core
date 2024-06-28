freenet/freenet-core

commit eda25027daf471c406ef8debe331149e3388c7c3

Bumpup tracing versions (#1138)

crates/core/src/tracing.rs
@@ -591,12 +591,11 @@ mod opentelemetry_tracer {
 
             let tracer = {
                 let tracer_provider = global::tracer_provider();
-                tracer_provider.versioned_tracer(
-                    "freenet",
-                    Some(env!("CARGO_PKG_VERSION")),
-                    Some("https://opentelemetry.io/schemas/1.21.0"),
-                    None,
-                )
+                tracer_provider
+                    .tracer_builder("freenet")
+                    .with_version(env!("CARGO_PKG_VERSION"))
+                    .with_schema_url("https://opentelemetry.io/schemas/1.21.0")
+                    .build()
             };
             let tx_bytes = transaction.as_bytes();
             let mut span_id = [0; 8];
@@ -671,6 +670,10 @@ mod opentelemetry_tracer {
             T: Into<std::borrow::Cow<'static, str>>,
         {
             unreachable!("shouldn't change span name")
+        }
+
+        fn add_link(&mut self, span_context: trace::SpanContext, attributes: Vec<KeyValue>) {
+            self.inner.add_link(span_context, attributes);
         }
     }
 
@@ -927,10 +930,11 @@ enum PutEvent {
 
 #[cfg(feature = "trace")]
 pub(crate) mod tracer {
+    use opentelemetry_otlp::WithExportConfig;
     use tracing::level_filters::LevelFilter;
     use tracing_subscriber::{Layer, Registry};
 
-    pub fn init_tracer(level: Option<LevelFilter>) -> anyhow::Result<()> {
+    pub fn init_tracer(level: Option<LevelFilter>, endpoint: Option<String>) -> anyhow::Result<()> {
         let default_filter = if cfg!(any(test, debug_assertions)) {
             LevelFilter::DEBUG
         } else {
@@ -972,12 +976,21 @@ pub(crate) mod tracer {
                 println!("setting OT collector with identifier: {identifier}");
                 let tracing_ot_layer = {
                     // Connect the Jaeger OT tracer with the tracing middleware
-                    // FIXME: remove
-                    #[allow(deprecated)]
-                    let ot_jaeger_tracer =
-                        opentelemetry_jaeger::config::agent::AgentPipeline::default()
-                            .with_service_name(identifier)
-                            .install_simple()?;
+                    let ot_jaeger_tracer = opentelemetry_otlp::new_pipeline()
+                        .tracing()
+                        .with_exporter(
+                            opentelemetry_otlp::new_exporter()
+                                .tonic()
+                                .with_endpoint(endpoint.unwrap_or_default()),
+                        )
+                        .with_trace_config(
+                            opentelemetry_sdk::trace::Config::default().with_resource(
+                                opentelemetry_sdk::Resource::new(vec![
+                                    opentelemetry::KeyValue::new(identifier, "tracing-jaeger"),
+                                ]),
+                            ),
+                        )
+                        .install_simple()?;
                     // Get a tracer which will route OT spans to a Jaeger agent
                     tracing_opentelemetry::layer().with_tracer(ot_jaeger_tracer)
                 };

crates/core/Cargo.toml
@@ -68,12 +68,13 @@ rsa = { version = "0.9", features = ["serde", "pem"] }
 pkcs8 = { version = "0.10", features = ["std", "pem"] }
 
 # Tracing deps
-opentelemetry = "0.22"
-opentelemetry-jaeger = { features = ["collector_client", "isahc", "rt-tokio"], optional = true, version = "0.21" }
+opentelemetry = "0.23"
+opentelemetry-jaeger = { features = ["collector_client", "isahc", "rt-tokio"], optional = true, version = "0.22" }
 tracing = { version = "0.1" }
-tracing-opentelemetry = { optional = true, version = "0.23.0" }
-tracing-subscriber = { optional = true, version = "0.3.16" }
-opentelemetry-otlp = { optional = true, version = "0.15" }
+tracing-opentelemetry = { optional = true, version = "0.24" }
+tracing-subscriber = { optional = true, version = "0.3" }
+opentelemetry-otlp = { optional = true, version = "0.16" }
+opentelemetry_sdk = { optional = true, version = "0.23", features = ["rt-tokio"] }
 
 # internal deps
 freenet-stdlib = { features = ["net"], workspace = true }

crates/core/src/bin/freenet.rs
@@ -48,7 +48,7 @@ async fn run_network(config: Config) -> anyhow::Result<()> {
 }
 
 fn main() -> anyhow::Result<()> {
-    freenet::config::set_logger(None);
+    freenet::config::set_logger(None, None);
     let config = ConfigArgs::parse().build()?;
     let rt = tokio::runtime::Builder::new_multi_thread()
         .worker_threads(

crates/core/src/config.rs
@@ -827,7 +827,7 @@ impl GlobalExecutor {
     }
 }
 
-pub fn set_logger(level: Option<tracing::level_filters::LevelFilter>) {
+pub fn set_logger(level: Option<tracing::level_filters::LevelFilter>, endpoint: Option<String>) {
     #[cfg(feature = "trace")]
     {
         static LOGGER_SET: AtomicBool = AtomicBool::new(false);
@@ -843,7 +843,7 @@ pub fn set_logger(level: Option<tracing::level_filters::LevelFilter>) {
             return;
         }
 
-        crate::tracing::tracer::init_tracer(level).expect("failed tracing initialization")
+        crate::tracing::tracer::init_tracer(level, endpoint).expect("failed tracing initialization")
     }
 }
 

crates/core/src/tracing/aof.rs
@@ -396,8 +396,6 @@ impl LogFile {
 mod tests {
     use std::time::SystemTime;
 
-    use tracing::level_filters::LevelFilter;
-
     use crate::{
         dev_tool::{PeerId, Transaction},
         tracing::NetEventLog,
@@ -408,7 +406,6 @@ mod tests {
     #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
     async fn read_write() -> anyhow::Result<()> {
         NEW_RECORDS_TS.get_or_init(SystemTime::now);
-        crate::config::set_logger(Some(LevelFilter::TRACE));
         let temp_dir = tempfile::tempdir()?;
         let log_path = temp_dir.path().join("event_log");
 
@@ -455,7 +452,6 @@ mod tests {
     #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
     async fn read_write_small() -> anyhow::Result<()> {
         NEW_RECORDS_TS.get_or_init(SystemTime::now);
-        crate::config::set_logger(Some(LevelFilter::TRACE));
         let temp_dir = tempfile::tempdir()?;
         let log_path = temp_dir.path().join("event_log");
 
@@ -502,7 +498,6 @@ mod tests {
     #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
     async fn read_write_truncate() -> anyhow::Result<()> {
         NEW_RECORDS_TS.get_or_init(SystemTime::now);
-        crate::config::set_logger(Some(LevelFilter::TRACE));
         let temp_dir = tempfile::tempdir()?;
         let log_path = temp_dir.path().join("event_log");
 

crates/fdev/src/main.rs
@@ -39,7 +39,7 @@ fn main() -> anyhow::Result<()> {
         .build()?;
     let config = Config::parse();
     if !config.sub_command.is_child() {
-        freenet::config::set_logger(None);
+        freenet::config::set_logger(None, None);
     }
     tokio_rt.block_on(async move {
         let cwd = std::env::current_dir()?;

crates/fdev/src/testing/network.rs
@@ -182,7 +182,7 @@ async fn start_child(config: &TestConfig, cmd_config: &NetworkProcessConfig) -> 
         "FREENET_PEER_ID",
         cmd_config.clone().id.expect("id should be set"),
     );
-    freenet::config::set_logger(None);
+    freenet::config::set_logger(None, None);
     if let Some(peer_id) = &cmd_config.id {
         let peer = NetworkPeer::new(peer_id.clone()).await?;
         peer.run(config, peer_id.clone()).await?;

commit 78cfabb20ec267a2eccbdc34bb4e0b373a60273a

Fix broken build
This line was introduced in #1138. WithExportConfig is not used anywhere.

crates/core/src/tracing.rs
@@ -930,7 +930,6 @@ enum PutEvent {
 
 #[cfg(feature = "trace")]
 pub(crate) mod tracer {
-    use opentelemetry_otlp::WithExportConfig;
     use tracing::level_filters::LevelFilter;
     use tracing_subscriber::{Layer, Registry};
 