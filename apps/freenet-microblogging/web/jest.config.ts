5e186f9ca50fafc000060045de2c4584b1f62d39
Hector Santos <netsirius@gmail.com>
2026-04-26 21:25:57 +0200

    refactor: remove freenet-microblogging app from monorepo (#3955)

--- apps/freenet-microblogging/web/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@jest/types';
-const config: Config.InitialOptions = {
-	verbose: true,
-	transform: {
-		"^.+\\.tsx?$": "ts-jest",
-	},
-	roots: ["<rootDir>/tests", "<rootDir>/src"]
-};
-export default config;
