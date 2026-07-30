8e82bd5bcb0824200d3a3bfdde73bafb68c1453a
Skye Soss <skye@soss.website>
2026-07-30 10:28:26 -0500

    fix(nix): use most recent tag, not main branch
    The freenet-autoupdate script previously ran the latest revision on the
    main branch. Now the script uses the most recent GitHub release.

--- flake.nix
+++ flake.nix
@@ -14,21 +14,27 @@
     {
       overlays.default = _: pkgs: {
         freenet = pkgs.callPackage (import ./package.nix) { };
-        freenet-autoupdate = pkgs.writeShellScriptBin "freenet-autoupdate" ''
-          trap "" INT
+        freenet-autoupdate = pkgs.writeShellApplication {
+          name = "freenet-autoupdate";
+          runtimeInputs = [ pkgs.gh pkgs.nix ];
+          text = ''
+            while true; do
+                vsn="$(gh release view --repo freenet/freenet-core \
+                  --json tagName --jq .tagName)"
+                echo "Running freenet $vsn"
+                exit_code=0
+                nix --option experimental-features 'nix-command flakes' \
+                  run "github:freenet/freenet-core/$vsn#freenet" || exit_code=$?
 
-          while true; do
-              nix run github:freenet/freenet-core#freenet
-              exit_code=$?
-
-              if [ $exit_code -eq 42 ]; then
-                  echo "Autoupdate triggered. Restarting..."
-              else
-                  echo "Non-autoupdate exit code: $exit_code. Stopping."
-                  break
-              fi
-          done
-        '';
+                if [ $exit_code -eq 42 ]; then
+                    echo "Autoupdate triggered. Restarting..."
+                else
+                    echo "Non-autoupdate exit code: $exit_code. Stopping."
+                    exit $exit_code
+                fi
+            done
+          '';
+        };
       };
     }
     // flake-utils.lib.eachDefaultSystem (
